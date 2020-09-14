0d4ac99ec06e2002cd997eb77aa96ed6e89fe6eb
gongpengjun <frank.gongpengjun@gmail.com>
2020-09-14 19:11:52 +0800

    Perl 命名捕获 named capture

---
+++ named_capture.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/envperl
+
+use strict;
+use warnings;
+
+my $names = 'Fred Flintstone and Wilma Flintstone';
+
+if ( $names =~ m/(?<last_name>\w+) and \w+ \g{last_name}/ ) {
+	print "I saw $+{last_name}\n";
+	#say "I saw $+{last_name}"; # needs `use 5.010`
+}
